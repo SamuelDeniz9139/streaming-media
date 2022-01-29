7dd92359f73c1e36f1d25906f685f220a0d439bd
Sam <sfd9139@g.rit.edu>
2022-01-29 15:06:24 -0500

    first commit

---
+++ src/htmlResponses.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+const page3 = fs.readFileSync(`${__dirname}/../client/client3.html`);
+const loadHTML = (request, response, page) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(page);
+  response.end();
+};
+const getIndex = (request, response) => {
+  loadHTML(request, response, index);
+};
+const getPage2 = (request, response) => {
+  loadHTML(request, response, page2);
+};
+const getPage3 = (request, response) => {
+  loadHTML(request, response, page3);
+};
+module.exports.getIndex = getIndex;
+module.exports.getPage2 = getPage2;
+module.exports.getPage3 = getPage3;
